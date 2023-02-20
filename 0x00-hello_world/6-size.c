354836b72e072b5070ac10e9a89a036fdff3afc2
Oboakoh <Oboakohgh@gmail.com>
2023-02-19 19:45:56 -0800

    done

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints "the size of various types of the computer it is 
+ *                compiled and run on."
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("Size of a char: 1 byte(s)");
+	printf("Size of an int: 4 byte(s)");
+	printf("Size of a long int: 4 byte(s)");
+	printf("Size of a long long int: 8 byte(s)");
+	printf("Size of a float: 4 byte(s)");
+
+	return (0);
+}
