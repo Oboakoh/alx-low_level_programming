736a4313804d1c32df2488287a72ed174d7cc756
Oboakoh <Oboakohgh@gmail.com>
2023-02-21 15:23:09 -0800

    fixed

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - "a program that prints _putchar, followed by a new line"
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("_putcha",\n);
+	return (0);
+
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+low level C programming for nested loops and functions
